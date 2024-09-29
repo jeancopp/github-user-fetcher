be6a8e83f78d878d85e751354bd23046dcd3cbf3
Jean Coppieters <jean.coppieters@kiwim.app>
2024-09-29 15:48:00 +0100

    Consuming and persisting data(with stack-tech)

--- src/service/FetchUserService.ts
+++ src/service/FetchUserService.ts
@@ -1,6 +1,28 @@
-
+import {fetchGitHubUser, fetchUserTechnologies} from "./GitHubService";
+import {insertUser} from "../repository/UserRepository";
+import {insertTechnology, linkUserToTechnology} from "../repository/TechnologyRepository";
 import {FetchUserDto} from "../dto/FetchUserDto";
 
 export default async function fetchUserService(userData: FetchUserDto) {
+    const user = await fetchGitHubUser(userData);
 
+    const savedUser = await insertUser(user);
+    console.trace(`User ${userData.username} stored in the database.`);
+
+    const technologies = await fetchUserTechnologies(userData);
+
+    for (const technology of technologies) {
+        const technologyId = await insertTechnology(
+            technology,
+        );
+
+        await linkUserToTechnology(
+            savedUser.id!,
+            technologyId,
+        );
+    }
+
+    console.trace(
+        `Technologies for user ${userData.username} stored in the database.`,
+    );
 }

5d3cfe84687417d8c42ca8780e5490214cb28410
Jean Coppieters <jean.coppieters@kiwim.app>
2024-09-29 14:52:00 +0100

    [WIP] Add db factory

---
+++ src/repository/db.ts
@@ -0,0 +1,15 @@
+import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+
+const pgp = pgPromise();
+const {parsed} = dotenv.config();
+
+const db = pgp({
+    host: parsed?.POSTGRES_HOST ?? "localhost",
+    port: parseInt(parsed?.POSTGRES_PORT ?? "5432"),
+    database: parsed?.POSTGRES_DB,
+    user: parsed?.POSTGRES_USER,
+    password: parsed?.POSTGRES_PASSWORD,
+});
+
+export default db;

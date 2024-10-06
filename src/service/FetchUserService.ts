82240dc397a0ba39a301852da49b9400e437a956
Jean Coppieters <jean.coppieters@kiwim.app>
2024-10-06 16:53:27 +0100

    feat: Updating insert user/stack flow after review

--- src/service/FetchUserService.ts
+++ src/service/FetchUserService.ts
@@ -1,32 +1,41 @@
 import {fetchGitHubUser, fetchUserTechnologies} from "./GitHubService";
 import {insertUser} from "../repository/UserRepository";
-import {insertTechnology, linkUserToTechnology} from "../repository/TechnologyRepository";
+import {
+  insertTechnology,
+  linkUserToTechnology
+} from "../repository/TechnologyRepository";
 import {FetchUserDto} from "../dto/FetchUserDto";
+import {UserTechnologies} from "../entity/User";
 
-export default async function fetchUserService(userData: FetchUserDto) {
+export default async function fetchUserService(userData: FetchUserDto)
+  : Promise<UserTechnologies | null> {
   const user = await fetchGitHubUser(userData);
   if (null === user) {
-    console.log("User not found!");
-    return;
+    return null;
   }
 
   const savedUser = await insertUser(user);
-  console.trace(`User ${userData.username} stored in the database.`);
-
   const technologies = await fetchUserTechnologies(userData);
 
-  for (const technology of technologies) {
-    const technologyId = await insertTechnology(
-      technology,
-    );
+  if (technologies.length > 0) {
+    for (const technology of technologies) {
+      const technologyId = await insertTechnology(
+        technology,
+      );
 
-    await linkUserToTechnology(
-            savedUser.id!,
-            technologyId,
+      await linkUserToTechnology(
+        savedUser.id!,
+        technologyId,
+      );
+    }
+
+    console.trace(
+      `Technologies for user ${userData.username} stored.`
     );
   }
 
-  console.trace(
-    `Technologies for user ${userData.username} stored in the database.`
-  );
+  return {
+    ...savedUser,
+    technologies,
+  }
 }
